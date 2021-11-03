708627cb27237997cc1ed3563816cfb38f05cc73
Christopher Harden <Harden.Christopher00@gmail.com>
2021-11-03 05:53:33 -0500

    finished basic tasks for day one of lab

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import { Component } from 'react';
 import axios from 'axios';
-
-
+import CityForm from './CityForm.js';
+import SearchCard from './SearchCard.js';
 
 export default class App extends Component {
     
     constructor(props){
         super(props)
         this.state = {
-            cityValue: ''
+            error: false,
+            location: {}
         }
     }
 
-    handleChange = (e) => {
-        this.setState({cityValue: e.target.value})
-    }
 
 
-    handleClick = async () => {
-        const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${this.state.cityValue}&format=json`;
+
+    getLocation = async (city) => {
+        const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${city}&format=json`;
         // key: YOUR_ACCESS_TOKEN
         // q: SEARCH_STRING
         // format: 'json'
         try {
         let response = await axios.get(url)
         
-        console.log(response.data);0
-        this.setState({location: response.data[]})
+        console.log(response.data[0]);
+        this.setState({location: response.data[0]}, this.getMapUrl)
         } catch (e) {
             console.error(e);
             this.setState({error: true})
@@ -36,14 +35,21 @@ export default class App extends Component {
     }
 
 
+    getMapUrl = () => {
+        let url = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${this.state.location.lat},${this.state.location.lon}&zoom=10`
+        this.setState({
+            location: {...this.state.location, map: url}
+        })
+    }
 
     render(){
         return (
             <>
-                <input onChange={this.handleChange} value={this.state.cityValue}/>
-                <p>{this.state.cityValue}</p>
+                <CityForm getLocation={this.getLocation}/>
+                {/* <input onChange={this.handleChange} value={this.state.cityValue}/> */}
+                <p>{this.state.cityName}</p>
                 <button onClick={this.handleClick}>SEARCH</button>
-                {this.state.location && <h1>{this.state.location.display_name }</h1>}
+                {this.state.location.map && <SearchCard location={this.state.location}/>}
             </>
         )
     }
